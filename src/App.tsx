e325862b05a1f56e6ebdac531c30757f5934bd67
joaopedroaat <joaopedroaat@outlook.com>
2024-05-01 14:51:39 -0300

    Remove prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <main className="flex h-screen items-center justify-center bg-pink-100">
         <section className="size-80 rounded-xl bg-pink-300 p-4 shadow-md">
           {/* Display */}
-          <div className="w-full rounded-xl bg-pink-200  y-2  px-4 text-right font-display text-4xl text-pink-100">
+          <div className="y-2 w-full  rounded-xl bg-pink-200 px-4 text-right font-display text-4xl text-pink-100">
             00
           </div>
         </section>
